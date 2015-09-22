4df03e8885fed2d0ed7d955e8555ff37690e30ec
yanchunguang <yanchunguang@xindong.com>
2015-09-22 19:05:54 +0800

    js继承实现

--- core/GClass.js
+++ core/GClass.js
@@ -1,10 +1,48 @@
 /**
  * Created by yanchunguang on 15/9/21.
+ * 自定义类继承
  */
 
-g.Class = function(){};
+(function(){
+    g.Class = function(){};
 
-g.Class.extend = function()
-{
+    g.Class.extend = function(props)
+    {
+        var _super = this.prototype;
+        var prototype = Object.create(_super);
 
-};
+        function Class() {
+            if (this.ctor)
+                this.ctor.apply(this, arguments);
+        }
+
+        var deepCopy = function(props,prototype)
+        {
+            var prototype = prototype || {};
+
+            for(var name in props)
+            {
+                if(typeof props[name] === "object")
+                {
+                    prototype[name] = (props[name].constructor === Array) ? [] : {};
+                    deepCopy(props[name], prototype[name]);
+                }
+                else
+                {
+                    prototype[name] = props[name];
+                }
+            }
+
+            return prototype;
+        };
+        prototype = deepCopy(props, prototype);
+
+        Class.prototype = prototype;
+
+        Class.extend = g.Class.extend;
+
+        return Class;
+    };
+
+})();
+
